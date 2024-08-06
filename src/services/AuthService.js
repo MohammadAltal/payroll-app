a7669220378b3a19c5b1189068aa4dae6a0d023b
Mohammad Altal <mohammad.altal@lyveglobal.com>
2024-08-07 01:23:15 +0400

    first commit

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,8 +3,14 @@
 class AuthService {
 
      isUserAuthenticated = () => {
-        const user = localStorage.getItem('user');
-        return user !== null;
+         const user = localStorage.getItem('user');
+         // Check if the user is not null and not an empty object
+         return user !== null && user !== '{}';
+    };
+
+    logoutUser = () => {
+        localStorage.setItem('user', JSON.stringify({}));
+        window.location.replace('/signin');
     };
 
 }
