b0b61ec855987385041bb6a656f8a343a9ee5ef4
Mohammad Altal <mohammad.altal@lyveglobal.com>
2024-08-08 01:53:20 +0400

    first commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -131,22 +131,9 @@ export default function Layout({ children }) {
                         <MainListItems />
                     </List>
                 </Drawer>
-                <Box
-                    component="main"
-                    sx={{
-                        backgroundColor: (theme) =>
-                            theme.palette.mode === 'light'
-                                ? theme.palette.grey[100]
-                                : theme.palette.grey[900],
-                        flexGrow: 1,
-                        height: '100vh',
-                        overflow: 'auto',
-                    }}
-                >
+                <Box component="main" sx={{flexGrow: 1, p: 3, height: '100vh', overflow: 'auto'}}>
                     <Toolbar />
-                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                        {children}
-                    </Container>
+                    {children}
                 </Box>
             </Box>
         </ThemeProvider>
