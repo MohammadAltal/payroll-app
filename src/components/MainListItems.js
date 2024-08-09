0aaf48700fda02e6dd30b06c31201740fe02ae2d
Mohammad Altal <mohammad.altal@lyveglobal.com>
2024-08-10 03:36:27 +0400

    Updates

--- src/components/MainListItems.js
+++ src/components/MainListItems.js
@@ -6,9 +6,21 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import {useNavigate} from "react-router-dom";
+import Collapse from '@mui/material/Collapse';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+import List from '@mui/material/List';
 
 export default function MainListItems() {
     const navigate = useNavigate();
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return (
         <React.Fragment>
             <ListItemButton onClick={() => { navigate('/dashboard'); }}>
@@ -17,18 +29,23 @@ export default function MainListItems() {
                 </ListItemIcon>
                 <ListItemText primary="Dashboard" />
             </ListItemButton>
-            <ListItemButton onClick={() => { navigate('/employees'); }}>
+            <ListItemButton onClick={handleClick}>
                 <ListItemIcon>
                     <PeopleIcon />
                 </ListItemIcon>
                 <ListItemText primary="Employees" />
+                {open ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
-            <ListItemButton onClick={() => { navigate('/employees/create'); }}>
-                <ListItemIcon>
-                    <PeopleIcon />
-                </ListItemIcon>
-                <ListItemText primary="Add Employee" />
-            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 4 }}  onClick={() => { navigate('/employees'); }}>
+                        <ListItemText  sx={{ paddingLeft: "40px" }} primary="List" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 4 }} onClick={() => { navigate('/employees/create'); }}>
+                        <ListItemText  sx={{ paddingLeft: "40px" }} primary="Create" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
 
             <ListItemButton onClick={() => { navigate('/salaries'); }}>
                 <ListItemIcon>
