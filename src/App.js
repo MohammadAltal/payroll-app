0fea3cf3eb2c4874c7ec7eca9c3a4a1e29ac131b
Mohammad Altal <mohammad.altal@lyveglobal.com>
2024-08-10 19:53:36 +0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Layout from "./components/Layout";
@@ -10,14 +11,29 @@ import SalariesOverview from './pages/Salaries/Overview';
 import ListPayments from './pages/Salaries/ListPayments';
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
-
+import AuthService from './services/AuthService';
 
 function App() {
+    const [redirectPath, setRedirectPath] = useState(null);
+    const authService = new AuthService;
+    const isAuthenticated = authService.isUserAuthenticated();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            // Redirect to Signin page if not authenticated
+            setRedirectPath("/signin");
+        } else {
+            // Redirect to Home page if authenticated
+            setRedirectPath("/home");
+        }
+    }, []);
+
     return (
         <div className="App">
             <main>
                 <Router>
                     <Routes>
+                        {redirectPath && <Route path="/" element={<Navigate to={redirectPath} />} />}
                         <Route path="/home" element={<Layout><ProtectedRoute element={Home} /></Layout>} />
                         <Route path="/employees" element={<Layout><ProtectedRoute element={ListEmployees} /></Layout>} />
                         <Route path="/employees/create" element={<Layout><ProtectedRoute element={CreateEmployee} /></Layout>} />
